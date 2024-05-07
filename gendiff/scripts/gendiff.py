309c34c5271120193e47ef566a0d09bc64f2090f
Evgeny <info@tinamagic.lv>
2024-05-08 00:07:04 +0300

    Step 1

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,19 +1,17 @@
-##!/usr/bin/env python3
+#!/usr/bin/env python3
 
-#from brain_games.cli import welcome_user
+# from brain_games.cli import welcome_user
 
 import argparse
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+    parser = argparse.ArgumentParser(description="Compares two configuration \
+     files and shows a difference.")
     parser.add_argument("first_file second_file")
 
     parser.parse_args()
 
 
-
 if __name__ == '__main__':
     main()
-
-
-
