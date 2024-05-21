a3443f064af33ee849fe1880181958aade30215b
Evgeny <info@tinamagic.lv>
2024-05-21 21:09:16 +0300

    Step 7

--- gendiff/tools/argparse.py
+++ gendiff/tools/argparse.py
@@ -6,6 +6,7 @@ def parser_arg():
     configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-f', '--format', default='stylish',
+                        help='set format of output')
     args = parser.parse_args()
     return args.first_file, args.second_file, args.format
