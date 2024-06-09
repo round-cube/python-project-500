a7a85bd08a095d2169a73c8b90e5197b290f340a
Evgeny <info@tinamagic.lv>
2024-06-09 13:46:49 +0300

    Step 8

--- gendiff/parse_file.py
+++ gendiff/parse_file.py
@@ -6,12 +6,12 @@ from pathlib import Path
 
 def get_dict_from_file(path_file):
     file_ext = Path(path_file).suffix
-    return open_file(path_file, file_ext)
+    path_f = Path() / 'tests/fixtures' / os.path.basename(path_file)
+    return open_file(path_f, file_ext)
 
 
 def open_file(path_file, file_ext):
-    path_file = os.path.basename(path_file)
-    with open(Path() / 'tests/fixtures' / path_file) as f:
+    with open(path_file) as f:
         if file_ext.lower() == '.json':
             return json.load(f)
         elif file_ext.lower() == '.yml' or file_ext.lower() == '.yaml':
